9cd5d9d6cac1b49a80a048438a9a279ae58c9d17
James Adam <james@lazyatom.com>
2012-06-06 16:26:41 +0100

    Simple initial app.
    Yo, I heard you liked apps, so I put an app in your app.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'nokogiri'
+gem 'multi_json'
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'open-uri'
+require 'multi_json'
+require 'date'
+
+def current_month
+  Date::MONTHNAMES[Date.today.month][0,3].downcase
+end
+
+get "/" do
+  url = "http://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=bbc_seasonal_foods_by_month&query=select%20*%20from%20%60swdata%60"
+  data = MultiJson.decode(open(url).read).inject({}) { |h, d| h[d["name"]] = d; h }
+
+  @now_in_season = MultiJson.decode(data[current_month]["now_in_season"])
+  @last_chance = MultiJson.decode(data[current_month]["last_chance"])
+  @now_out_of_season = MultiJson.decode(data[current_month]["now_out_of_season"])
+  
+  @featured = @now_in_season.shuffle.first
+  @now_in_season.delete(@featured)
+
+  erb :publication
+end
