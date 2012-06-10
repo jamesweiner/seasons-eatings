fbd3eb46d469108c49d41c0dd894284098fb709e
James Adam <james@lazyatom.com>
2012-06-10 12:42:12 +0100

    Try showing food lists as sentences.

--- app.rb
+++ app.rb
@@ -7,6 +7,15 @@ def current_month
   Date::MONTHNAMES[Date.today.month][0,3].downcase
 end
 
+helpers do
+  def to_sentence(list)
+    list.map! {|i| "<span class='item'>#{i}</span>" }
+    last_ones = list.pop, list.pop
+    list.push(last_ones.compact.reverse.join(" and "))
+    list.join(", ")
+  end
+end
+
 get "/" do
   url = "http://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=bbc_seasonal_foods_by_month&query=select%20*%20from%20%60swdata%60"
   data = MultiJson.decode(open(url).read).inject({}) { |h, d| h[d["name"]] = d; h }
